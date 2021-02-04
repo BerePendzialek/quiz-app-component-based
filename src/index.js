e65218041936b05e8b3a95baa7431f75d1246fa6
Berenice Pendzialek <berenice.pendzialek@gmail.com>
2021-02-04 13:57:09 +0100

    add header component with js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import Button from './components/Button'
+import Header from './components/Header'
 
 const button = Button('Click me!')
+const header = Header
 
 document.body.append(button)
+document.body.append(header)
