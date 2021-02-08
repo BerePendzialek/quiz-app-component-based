9095d4c6b4fa323882d38055293ff1fd8be4293b
Berenice Pendzialek <berenice.pendzialek@gmail.com>
2021-02-08 15:16:52 +0100

    add createForm element

---
+++ src/components/CreateForm.js
@@ -0,0 +1,24 @@
+import createElement from '../lib/createElement'
+import './CreateForm.css'
+
+export default function CreateForm() {
+  const el = createElement('form', {
+    className: 'CreateForm',
+    innerHTML: `
+    <label>
+    Question:
+<input name="question" placeholder="What is...?">
+    </label>
+    <label>
+    Answer:
+<input name="question" placeholder="What is...?">
+    </label>
+    <label>
+    Tags:
+<input name="question" placeholder="Tags separated by comma?">
+    </label>
+    <button>Create question</button>
+    `,
+  })
+  return el
+}
